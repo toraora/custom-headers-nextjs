06ddd1ef8c95c3fe4cb2dee4ae74f0e85c92f92e
Nathan Wong <nathan@berkeley.edu>
2023-08-31 20:12:57 -0700

    add custom headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: async () => {
+    return [{source: '/', headers: [{key: 'X-Content-Type-Options', value: 'nosniff'}, {key: 'content-type', value: 'application/custom'}]}];
+  }
 }
 
 module.exports = nextConfig
